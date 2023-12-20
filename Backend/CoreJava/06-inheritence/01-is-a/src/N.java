155268b37932c0d108427447b4b565230a3beeb1
g13m13 <g13m13@icloud.com>
2023-12-20 19:42:08 +0530

    super-calling-statement-usage-in-constructor

---
+++ Backend/CoreJava/06-inheritence/01-is-a/src/N.java
@@ -0,0 +1,16 @@
+class M 
+{
+	M()
+	{
+		System.out.println("M()");
+	}
+}
+class N extends M
+{
+
+	public static void main(String[] args) 
+	{
+		N n1 = new N();
+		System.out.println("from N main");
+	}
+}
